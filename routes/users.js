3af33112fc2122b7299f441423d4c0d1b419b9f7
jiajun.chpu <jiajun.chou@104.com.tw>
2019-01-03 18:11:55 +0800

    GET API: req.query

--- routes/users.js
+++ routes/users.js
@@ -16,6 +16,7 @@ router.get('/cars', function(req, res, next) {
     res.send('Hello Dynamodb will coming soon!');
 });
 
+// Get method: params
 router.get('/readCar/:id', function(req, res) {
 
     var paramId = parseInt(req.params.id);
@@ -38,6 +39,33 @@ router.get('/readCar/:id', function(req, res) {
             res.send(JSON.stringify(data, null, 2));
         }
     });
-})
+});
+
+// Get method: query
+router.get('/readCar', function(req, res) {
+
+    var paramId = parseInt(req.query.id);
+
+    var docClient = new AWS.DynamoDB.DocumentClient();
+    var table = "CarStorage";
+
+    var params = {
+        TableName: table,
+        Key:{
+            id: paramId
+        }
+    };
+
+    docClient.get(params, function(err, data) {
+        if (err) {
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+            res.send(JSON.stringify(data, null, 2));
+        }
+    });
+});
+
+
 
 module.exports = router;
