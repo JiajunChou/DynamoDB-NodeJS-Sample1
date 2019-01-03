a140421aa5090a690a588d84786f59df856c0a20
jiajun.chpu <jiajun.chou@104.com.tw>
2019-01-03 16:34:30 +0800

    Dynamodb crud: Update

---
+++ DynamoDB/crud/updateItem.js
@@ -0,0 +1,30 @@
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+    region: "us-west-2"
+  });
+  
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+var table = "CarStorage";
+
+var params = {
+    TableName:table,
+    Key:{
+        "id": 3
+    },
+    UpdateExpression: "set description = :n",
+    ExpressionAttributeValues:{
+        ":n":"bumbobee"
+    },
+    ReturnValues:"UPDATED_NEW"
+};
+
+console.log("Updating the item...");
+docClient.update(params, function(err, data) {
+    if (err) {
+        console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("UpdateItem succeeded:", params.id);
+    }
+});
